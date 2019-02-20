390b2c5a7472822361753172aac8e00636046308
jcope <thejemmeh@gmail.com>
2019-02-19 17:07:17 -0800

    Shorten print_bits

--- 2-print_bits/print_bits.c
+++ 2-print_bits/print_bits.c
@@ -1,63 +1,14 @@
-//Pass, but this is sloppy, I should use a shorter solution. 
+// For this solution you need to understand:
+// - Bit Shifting
+// - Bitwise operators
 
 #include <unistd.h>
+
 void	print_bits(unsigned char octet)
 {
-	if(octet >= 128){
-		write(1, "1", 1);
-		octet -= 128;
-	}
-	else
-		write(1, "0", 1);
-
-	if(octet >= 64){
-		write(1, "1", 1);
-		octet -= 64;
-	}
-	else
-		write(1, "0", 1);
-
-	if(octet >= 32){
-		write(1, "1", 1);
-		octet -= 32;
-	}
-	else
-		write(1, "0", 1);
-
-	if(octet >= 16){
-		write(1, "1", 1);
-		octet -= 16;
-	}
-	else
-		write(1, "0", 1);
-		
-	if(octet >= 8){
-		write(1, "1", 1);
-		octet -= 8;
-	}
-	else
-		write(1, "0", 1);
-
-	if(octet >= 4){
-		write(1, "1", 1);
-		octet -= 4;
-	}
-	else
-		write(1, "0", 1);	
-
-	if(octet >= 2){
-		write(1, "1", 1);
-		octet -= 2;
-	}
-	else
-		write(1, "0", 1);	
-
-	if(octet >= 1){
-		write(1, "1", 1);
-		octet -= 1;
-	}
-	else
-		write(1, "0", 1);	
+	int	i = 256;
+	while (i >>= 1)
+		(octet & i) ? write(1, "1", 1) : write(1, "0", 1);
 }
 
 /* Testing Only
@@ -70,6 +21,8 @@ int		main(void){
 	print_bits(128);
 		printf("\n");
 	print_bits(27);
+	print_bits('a');
+	printf("\n");
 	return(0);
 }
 
